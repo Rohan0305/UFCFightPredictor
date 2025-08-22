8c276ba4ad85521b418ae6af72387cd47ef51bc0
Rohan0305 <rohanvallapalli@gmail.com>
2025-08-22 11:19:20 -0400

    Remove environment

--- ufc-fight-predictor/src/app/app.component.ts
+++ ufc-fight-predictor/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
